26651da10f1885db13586d02a358df1a46e30920
Taha <taha@wabisabi.ltd>
2024-11-26 16:16:51 +0000

    refactor(app): domioes

--- src/dominoes.rs
+++ src/dominoes.rs
@@ -3,19 +3,14 @@ pub fn chain(input: &[(u8, u8)]) -> Option<Vec<(u8, u8)>> {
         return Some(vec![]);
     }
 
-    let mut used = vec![false; input.len()];
-    let mut current_chain = Vec::new();
-
     for i in 0..input.len() {
+        let mut used = vec![false; input.len()];
+        let mut current_chain = vec![input[i]];
         used[i] = true;
-        current_chain.push(input[i]);
 
         if build_chain(input, &mut used, &mut current_chain) {
             return Some(current_chain);
         }
-
-        used[i] = false;
-        current_chain.pop();
     }
 
     None
@@ -32,27 +27,18 @@ fn build_chain(
 
     let current_end = current_chain.last().unwrap().1;
 
-    for i in 0..input.len() {
-        if !used[i] {
-            let domino = input[i];
+    for (i, &domino) in input.iter().enumerate() {
+        if !used[i] && (domino.0 == current_end || domino.1 == current_end) {
+            let next_domino = if domino.0 == current_end { domino } else { (domino.1, domino.0) };
+            current_chain.push(next_domino);
+            used[i] = true;
 
-            if domino.0 == current_end || domino.1 == current_end {
-                let next_domino = if domino.0 == current_end {
-                    domino
-                } else {
-                    (domino.1, domino.0)
-                };
-
-                current_chain.push(next_domino);
-                used[i] = true;
-
-                if build_chain(input, used, current_chain) {
-                    return true;
-                }
-
-                current_chain.pop();
-                used[i] = false;
+            if build_chain(input, used, current_chain) {
+                return true;
             }
+
+            current_chain.pop();
+            used[i] = false;
         }
     }
 
